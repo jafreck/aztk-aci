85a050e0376561c1514508f4234a21c4e05c57f8
Jake Fredk <jafreck@microsoft.com>
2018-06-05 23:56:37 -0700

    whitespace

--- src/models/service_principal.py
+++ src/models/service_principal.py
@@ -3,10 +3,9 @@ class ServicePrincipal:
         self.client_id = None
         self.tenant_id = None
         self.credential = None
-    
+
     def from_dict(self, service_principal_dict):
         self.client_id = service_principal_dict['client_id']
         self.tenant_id = service_principal_dict['tenant_id']
         self.credential = service_principal_dict['credential']
         return self
-        
