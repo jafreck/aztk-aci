490e61cf357d189e68f71977bdb6c7985a31c083
Jake Fredk <jafreck@microsoft.com>
2018-06-05 22:54:17 -0700

    deploy updates

--- src/deploy.py
+++ src/deploy.py
@@ -6,11 +6,14 @@ from azure.mgmt.containerinstance.models import (
     Container, ContainerGroup, ContainerGroupNetworkProtocol, ContainerPort,
     IpAddress, OperatingSystemTypes, Port, ResourceRequests,
     ResourceRequirements, EnvironmentVariable)
+from azure.mgmt.resource.resources.models import ResourceGroup, ResourceGroupProperties
+from azure.mgmt.resource.resources.resource_management_client import ResourceManagementClient
 
 
-def create_container_group(aci_client, resource_group, container_group_name,
-                           container_image_name):
-    """Creates a container group with a single container.
+def create_container_group_multi(aci_client, resource_group,
+                                 container_group_name, containers):
+    """Creates a container group with two containers in the specified
+       resource group.
 
     Arguments:
         aci_client {azure.mgmt.containerinstance.ContainerInstanceManagementClient}
@@ -19,29 +22,21 @@ def create_container_group(aci_client, resource_group, container_group_name,
                     -- The resource group in which to create the container group.
         container_group_name {str}
                     -- The name of the container group to create.
-        container_image_name {str}
-                    -- The container image name and tag, for example:
-                       microsoft\aci-helloworld:latest
+        containers {List[azure.mgmt.containerinstance.Container]}
     """
     print("Creating container group '{0}'...".format(container_group_name))
 
-    # Configure the container
-    container_resource_requests = ResourceRequests(memory_in_gb=1, cpu=1.0)
+    # Configure the containers
+    container_resource_requests = ResourceRequests(memory_in_gb=2, cpu=1.0)
     container_resource_requirements = ResourceRequirements(
         requests=container_resource_requests)
-    container = Container(
-        name=container_group_name,
-        image=container_image_name,
-        resources=container_resource_requirements,
-        ports=[ContainerPort(port=80)])
 
     # Configure the container group
     ports = [Port(protocol=ContainerGroupNetworkProtocol.tcp, port=80)]
-    group_ip_address = IpAddress(
-        ports=ports, dns_name_label=container_group_name)
+    group_ip_address = IpAddress(ports, dns_name_label=container_group_name)
     group = ContainerGroup(
         location=resource_group.location,
-        containers=[container],
+        containers=containers,
         os_type=OperatingSystemTypes.linux,
         ip_address=group_ip_address)
 
@@ -56,6 +51,7 @@ def create_container_group(aci_client, resource_group, container_group_name,
     print("Once DNS has propagated, container group '{0}' will be reachable at"
           " http://{1}".format(container_group_name,
                                container_group.ip_address.fqdn))
+    return container_group
 
 
 def create_container(*args, **kwargs):
@@ -63,10 +59,10 @@ def create_container(*args, **kwargs):
         kwargs.get('name'),
         kwargs.get('image'),
         kwargs.get('resources'),
-        command=None,
-        ports=None,
-        environment_variables=None,
-        volume_mounts=None)
+        command=kwargs.get('command'),
+        ports=kwargs.get('ports'),
+        environment_variables=kwargs.get('environment_variables'),
+        volume_mounts=kwargs.get('volume_mounts'))
 
 
 def create_master_container(*args, **kwargs):
@@ -74,10 +70,10 @@ def create_master_container(*args, **kwargs):
         kwargs.get('name'),
         kwargs.get('image'),
         kwargs.get('resources'),
-        command=None,
-        ports=None,
-        environment_variables=None,
-        volume_mounts=None)
+        command=kwargs.get('command'),
+        ports=kwargs.get('ports'),
+        environment_variables=kwargs.get('environment_variables'),
+        volume_mounts=kwargs.get('volume_mounts'))
 
 
 def create_worker_container(*args, **kwargs):
@@ -85,10 +81,10 @@ def create_worker_container(*args, **kwargs):
         kwargs.get('name'),
         kwargs.get('image'),
         kwargs.get('resources'),
-        command=None,
-        ports=None,
-        environment_variables=None,
-        volume_mounts=None)
+        command=kwargs.get('command'),
+        ports=kwargs.get('ports'),
+        environment_variables=kwargs.get('environment_variables'),
+        volume_mounts=kwargs.get('volume_mounts'))
 
 
 def create_extension_container(*args, **kwargs):
@@ -96,27 +92,59 @@ def create_extension_container(*args, **kwargs):
         kwargs.get('name'),
         kwargs.get('image'),
         kwargs.get('resources'),
-        command=None,
-        ports=None,
-        environment_variables=None,
-        volume_mounts=None)
+        command=kwargs.get('command'),
+        ports=kwargs.get('ports'),
+        environment_variables=kwargs.get('environment_variables'),
+        volume_mounts=kwargs.get('volume_mounts'))
 
 
 def create_spark_cluster(*args, **kwargs):
-    master_container = create_master_container()
+    master_container = [create_master_container()]
     worker_containers = [
-        create_worker_container() for i in kwargs.get("worker_count")
+        create_worker_container() for _ in range(kwargs.get("worker_count"))
     ]
+    containers = master_container.extend(worker_containers)
 
-    cg = create_container_group()
-    
+    resource_group = get_resource_group(
+        kwargs.get('resource_management_client'),
+        kwargs.get('resource_group_name'))
+
+    cg = create_container_group_multi(
+        aci_client=kwargs.get('aci_client'),
+        resource_group=resource_group,
+        container_group_name=kwargs.get('cluster_name'),
+        containers=containers,
+    )
+    return cg
+
+
+#TODO: change to get_or_create_resource_group
+def get_resource_group(resource_management_client, resource_group_name):
+    if resource_management_client.resource_groups.check_existence(
+            resource_group_name):
+        return resource_management_client.resource_groups.get(
+            resource_group_name)
+    else:
+        # TODO: create resource group
+        pass
 
 
 def create_aci_client(secrets):
     credentials = ServicePrincipalCredentials(
         client_id=secrets.client_id, secret=secrets.secret)
     return ContainerInstanceManagementClient(
-        credentials, secrets.subscription_id, base_url=None)
+        credentials,
+        secrets.subscription_id,
+        base_url=None,
+    )
+
+
+def create_resource_management_client(secrets):
+    credentials = ServicePrincipalCredentials(
+        client_id=secrets.client_id,
+        secret=secrets,
+    )
+    return ResourceManagementClient(credentials, secrets.subscription_id)
 
 
 def read_secrets(secrets_file):
@@ -128,4 +156,13 @@ if __name__ == "__main__":
     secrets_path = os.path.join(
         os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
         "secrets.yaml")
-    client = create_aci_client(read_secrets(secrets_path))
+    secrets = read_secrets(secrets_path)
+    aci_client = create_aci_client(secrets)
+    resource_management_client = create_resource_management_client(secrets)
+
+    cluster = create_spark_cluster(
+        worker_count=2,
+        resource_management_client=resource_management_client,
+        aci_client=aci_client,
+        resource_group_name="spark-aci",
+        cluster_name="test")
